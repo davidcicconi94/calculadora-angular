5342e03cfccb2d16150fcfff43c6b52d30bfa0b6
David <david.cicconi94@gmail.com>
2021-12-02 22:20:40 -0300

    First Angular simple proyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculadora-angular';
+
+  numberOne: number = 0;
+  numberTwo: number = 0;
+  resultado: number = 0;
+
+  sumar(): void {
+    this.resultado = this.numberOne + this.numberTwo;
+  }
+
+  restar(): void {
+    this.resultado = this.numberOne - this.numberTwo;
+  }
+
+  multiplicar(): void {
+    this.resultado = this.numberOne * this.numberTwo;
+  }
+
+  dividir(): void {
+    if (this.numberTwo == 0) {
+      alert('ERROR: el divisor no puede ser 0!< ');
+    } else {
+      this.resultado = this.numberOne / this.numberTwo;
+    }
+  }
 }
